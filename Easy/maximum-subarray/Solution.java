d9d24609afeb3a8baefdd136b9d56acaf9d7d14a
Lei Wang wanly3643@outlook.com <wanly3643@outlook.com>
2017-11-09 14:40:29 +0800

    Test

---
+++ Easy/maximum-subarray/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int start = -1;
+        int end = nums.length;
+        int i = start + 1;
+        int j = end - 1;
+        int sum = 0;
+        int min = 0;
+
+        while (i < j) {
+            if (nums[i] < nums[j]) {
+                sum += nums[i];
+
+                if (sum <= min) {
+                    min = sum;
+                    start = i;
+                }
+                i ++;
+            } else {
+                sum += nums[j];
+
+                if (sum <= min) {
+                    min = sum;
+                    end = j;
+                }
+                j --;
+            }
+        }
+
+        System.out.println(start + "-" + end);
+        
+        int maxSum = 0;
+        for (i = start + 1; i < end; i ++) {
+            maxSum += nums[i];
+        }
+
+        return maxSum;
+    }
+}
